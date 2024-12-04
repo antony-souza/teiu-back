687b6d18684b259e2d777072215df833f57ec12c
antony-souza <antonygustavo09@hotmail.com>
2024-12-04 17:12:55 -0300

    Criando o service do Sales e o Controller

--- src/services/sales/sales.service.ts
+++ src/services/sales/sales.service.ts
@@ -1,26 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSaleDto } from './dto/create-sale.dto';
-import { UpdateSaleDto } from './dto/update-sale.dto';
+import { SalesRepository } from 'src/repositories/sales.repository';
 
 @Injectable()
 export class SalesService {
-  create(createSaleDto: CreateSaleDto) {
-    return 'This action adds a new sale';
+  constructor(private readonly salesRepository: SalesRepository) {}
+
+  async createSales(dto: CreateSaleDto) {
+    const checkQuantityProduct = await this.salesRepository.checkStock(
+      dto.product_id,
+    );
+
+    if (!checkQuantityProduct) {
+      throw new NotFoundException('Product not found');
+    }
+
+    if (checkQuantityProduct.quantity < dto.quantity_sold) {
+      throw new NotFoundException('Insufficient stock');
+    }
+
+    await this.salesRepository.updateStock(dto.product_id, dto.quantity_sold);
+
+    return await this.salesRepository.createSale(dto);
   }
 
   findAll() {
     return `This action returns all sales`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} sale`;
-  }
-
-  update(id: number, updateSaleDto: UpdateSaleDto) {
-    return `This action updates a #${id} sale`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} sale`;
   }
 }
